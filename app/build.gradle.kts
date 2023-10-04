57fe91d60869fd98244a9d5b6d3a452e3fc47a8c
tolyakulak <anatoliyid0207@gmail.com>
2023-10-04 23:26:45 +0300

    Updated version to 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.github.tolyakulak.discordspammer"
         minSdk = 29
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
